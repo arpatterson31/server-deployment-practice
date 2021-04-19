b6bbe0345bc87cc92a7e3660e99cb42e9870e40e
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-04-19 16:13:18 -0700

    initial setup for lab completed on dev branch

--- handlers/404.js
+++ handlers/404.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = (req, res) => {
-  res.status(404).send( {
+  res.status(404).send({
     error: 404,
     route: req.path,
     message: 'route not found'
--- server.js
+++ server.js
@@ -3,11 +3,30 @@
 const express = require('express');
 const app = express();
 
+const stamper = require('./middleware/stamper.js');
 const notFoundHandler = require('./handlers/404.js');
-const errors = require('./handlers/500.js');
+const errorHandler = require('./handlers/500.js');
+
+app.get('/', stamper, (req, res) => {
+  res.status(200).send('Hey girl, hey!')
+});
+
+app.get('/data', stamper, (req, res) => {
+  let output = {
+    time: req.timestamp,
+    10: 'even',
+    5: 'odd'
+  }
+
+  res.status(200).json(output);
+});
+
+app.get('/bad', (req, res, next) => {
+  next('try again!'); // when you pass next anything, you are nexting this to your error
+});
 
 app.use('*', notFoundHandler);
-app.use(errors);
+app.use(errorHandler);
 
 function start(port) {
   app.listen(port, () => {
